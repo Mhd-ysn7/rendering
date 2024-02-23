f18907c7fffb10a88a1cb3049112736d35843393
crist <cristianocr7thebbc@gmail.com>
2024-02-23 17:57:59 +0200

    2nd commit

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ app.get("/",function(req,res){
 res.send("Hello World");
 })
 
+app.get("/students",function(req,res){
+    var data=[
+        {id:1,name:"Mohamad Yassine"},
+        {id:2,name:"Ahmad Kassem"},
+        {id:3,name:"Hassan Baalbaki"},
+        {id:4,name:"Bilal Hayek"},
+        {id:5,name:"Pierre Mahdi"}
+    ]
+    res.send(data);
+    })
+    
 app.listen(PORT)
