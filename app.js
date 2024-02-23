fd5ca245edd95ee774322ce35b0aa0780d0139ca
crist <cristianocr7thebbc@gmail.com>
2024-02-23 18:27:05 +0200

    3rd commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express();
 const PORT= process.env.PORT || 3000
-
+var cors = require('cors')
+app.use(cors())
 app.get("/",function(req,res){
 res.send("Hello World");
 })
